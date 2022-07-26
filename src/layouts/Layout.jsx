99f84fbd9fe68528bbb07cefe2e88ffb53e75c74
Sebastián Gómez <sebastian.gomezr11@gmail.com>
2022-07-25 21:11:29 -0500

    FEAT: Add styles to footer

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -8,8 +8,10 @@ const Layout = () => (
   <main className='h-screen flex flex-col'>
     <div className='h-full overflow-y-auto'>
       <Outlet />
+      <footer className='bg-gradient-to-b from-main-purple via-violet to-violet p-3 text-white'>
+        © 2022 - Sebastián Gómez Ramírez
+      </footer>
     </div>
-    <footer className='bg-gray-200 p-3'>footer</footer>
   </main>
 );
 
